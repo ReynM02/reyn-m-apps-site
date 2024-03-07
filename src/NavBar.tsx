b35d9a3fb85039c8d560b1fcfd62183ed18acd63
MatthewR-SVL <matt.reynolds@smartvisionlights.com>
2024-03-07 18:30:01 -0500

    add images

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -2,17 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import icon from './imgs/transparent_icon192.png';
 
 function NavBar() {
   return (
-    <Navbar data-bs-theme="dark" expand="lg" className="navBar">
+    <Navbar data-bs-theme="dark" className="navBar" sticky="top">
       <Container>
-        <Navbar.Brand href="/">ReynMApps</Navbar.Brand>
+      <Navbar.Brand href="/">
+            <img
+              alt=""
+              src={icon}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            ReynMApps
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Projects" id="basic-nav-dropdown">
+            <NavDropdown title="Projects" id="basic-nav-dropdown" className="navBar">
               <NavDropdown.Item href="/projects/tip-calculator">Tip Calculator</NavDropdown.Item>
               <NavDropdown.Item href="/projects/catch-the-object">Catch The Object</NavDropdown.Item>
             </NavDropdown>
